dcdb729a7435aabe3fded3fd866b78a804d0efdf
LinShan-Bin <zhangchubin1@gmail.com>
2022-05-16 20:58:14 +0800

    将 logits 保存下来

--- generate_test_json.py
+++ generate_test_json.py
@@ -36,25 +36,33 @@ def get_json(model):
             logits = model(imgs * masks)
             # softmaxed = F.softmax(logits, dim=1) * muls
             # softmaxed = softmaxed.detach().cpu().numpy()
-            softmaxed = (logits * muls).detach().cpu().numpy()
-            
+            # softmaxed = (logits * muls).cpu().numpy()
+            logits = logits.cpu().numpy()
             # 生成标签
             for i in range(len(img_ids)):
+                # pid = pro_id[i]
+                # id = img_ids[i]
+                # name = img_ns[i]
+                # sf = softmaxed[i]
+                # opt_tags = test_json[pid]['optional_tags']
+                # prob = np.zeros(len(opt_tags))  # 不一定是“真概率”
+                # for i, tag in enumerate(opt_tags):
+                #     emb = embed(tag)
+                #     if len(emb) == 0:
+                #         prob[i] = 10
+                #         # 这么定是希望模型做排除法，其他颜色概率不大时选择未知的标签。10 是尝试出来效果比较好的值。
+                #     else:
+                #         prob[i] = np.mean(sf[emb])  # 一个标签也适用
+                # tag_id = np.argmax(prob)
+                # test_json[pid]['imgs_tags'][id][name] = opt_tags[tag_id]
+                logit_i = ''
+                for j in range(CLASS_NUM):
+                    logit_i = logit_i + '{:.6f} '.format(logits[i][j].item())
                 pid = pro_id[i]
                 id = img_ids[i]
                 name = img_ns[i]
-                sf = softmaxed[i]
-                opt_tags = test_json[pid]['optional_tags']
-                prob = np.zeros(len(opt_tags))  # 不一定是“真概率”
-                for i, tag in enumerate(opt_tags):
-                    emb = embed(tag)
-                    if len(emb) == 0:
-                        prob[i] = 10
-                        # 这么定是希望模型做排除法，其他颜色概率不大时选择未知的标签。10 是尝试出来效果比较好的值。
-                    else:
-                        prob[i] = np.mean(sf[emb])  # 一个标签也适用
-                tag_id = np.argmax(prob)
-                test_json[pid]['imgs_tags'][id][name] = opt_tags[tag_id]
+                test_json[pid]['imgs_tags'][id][name] = logit_i
+
 
     with open('./result.json', 'w+', encoding='utf-8') as f:
         json.dump(test_json, f, ensure_ascii=False, indent=4)
@@ -64,7 +72,7 @@ if __name__ == '__main__':
     # For MaskedConvX_tiny
     model = convnext_tiny(pretrained=False)
     model.classifier[2] = nn.Linear(768, CLASS_NUM)
-    check_point = torch.load('./checkpoint_e8(best).pth')
+    check_point = torch.load('checkpoint_e8(best).pth')
     state_dict = check_point['model_state_dict']
     model.load_state_dict(state_dict)
     get_json(model)
